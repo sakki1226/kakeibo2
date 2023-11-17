7fc40e1e838d5de18d4054c708b948a5abd28821
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-11-17 21:12:19 +0900

    application_job.rbの修正

--- app/mailers/jobs/application_job.rb
+++ app/mailers/jobs/application_job.rb
@@ -1,4 +1,4 @@
-class ApplicationJob < ActiveJob::Base
+class Jobs::ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
 
