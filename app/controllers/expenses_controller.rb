bef97195d2c06d19d14c4166165c9643c6ecd93b
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2024-01-07 16:42:54 +0900

    メディアクエリ5

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,6 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
 
   def index
